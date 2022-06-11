c1f0f4b85ad2d6e33fcccfc067459c58dc225aef
Kathleen Ly <heykathl@users.noreply.github.com>
2022-06-11 21:11:33 +0100

    Moved deleteItem from component to actions and reducer

--- client/src/actions/itemActions.js
+++ client/src/actions/itemActions.js
@@ -7,3 +7,10 @@ export const getItems = () => {
     type: GET_ITEMS
   }
 }
+
+export const deleteItem = (id) => {
+  return {
+    type: DELETE_ITEM,
+    payload: id
+  }
+}
--- client/src/components/ShoppingList.js
+++ client/src/components/ShoppingList.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { Container, ListGroup, ListGroupItem, Button } from 'reactstrap';
 import { CSSTransition, TransitionGroup } from 'react-transition-group';
-import { v4 as uuid } from 'uuid';
 import { connect } from 'react-redux';
-import { getItems } from '../actions/itemActions';
+import { getItems, deleteItem } from '../actions/itemActions';
 import PropTypes from 'prop-types';
+import { v4 as uuid } from 'uuid';
 
 class ShoppingList extends Component {
   
   componentDidMount() {
     this.props.getItems();
+  }
+
+  onDeleteCLick = (id) => {
+    this.props.deleteItem(id)
   }
 
   render() {
@@ -38,11 +42,7 @@ class ShoppingList extends Component {
                     className="remove-btn" 
                     color="danger"
                     size="sm"
-                    onClick={() => {
-                      this.setState(state => ({
-                        items: state.items.filter(item => item.id != id)
-                      }))
-                    }}>&times;</Button>
+                    onClick={this.onDeleteCLick.bind(this, id)}>&times;</Button>
                   {name}
                 </ListGroupItem>
               </CSSTransition>
@@ -63,5 +63,5 @@ const mapStateToProps = (state) => ({
   item: state.item
 })
 
-export default connect(mapStateToProps, { getItems })(ShoppingList);
+export default connect(mapStateToProps, { getItems, deleteItem })(ShoppingList);
 
